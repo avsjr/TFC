a51d2a1c2f24c23c1ae29bda07ef608a22f0fc0f
antonio <avsj@me.com>
2023-07-26 12:20:35 -0300

    alteracoes

--- Holt-Winters.R
+++ Holt-Winters.R
@@ -1,15 +1,42 @@
+# Instalando a biblioteca forecast
 install.packages("forecast")
 
+# Carregando a biblioteca forecast
 library(forecast)
 
-ano <- c(2005, 2007, 2009, 2011, 2013, 2015, 2017, 2019)
-indice_aprovacao <- c(0.70, 0.74, 0.83, 0.87, 0.91, 0.92, 0.93, 0.95)
+# Dados fornecidos
+ano <- c(2005, 2007, 2009, 2011, 2013, 2015, 2017, 2019, 2021)
+indice_aprovacao <- c(0.70, 0.74, 0.83, 0.87, 0.91, 0.92, 0.93, 0.95, 0.97)
+saebe <- c(4.92, 5.14, 5.44, 5.53, 5.71, 5.54, 5.69, 5.65, 5.15)
 
-serie_temporal <- ts(indice_aprovacao, start = min(ano), frequency = 1)
+# Criando um data frame com os dados
+data <- data.frame(Ano = ano, Indice_Aprovacao = indice_aprovacao, SAEBE = saebe)
 
-modelo_hw <- forecast::hw(serie_temporal, seasonal = "additive")
+# Ajuste do modelo de suavização exponencial simples para "Indice Aprovação (P)"
+model_aprovacao <- ets(data$Indice_Aprovacao, model = "ANN")
 
-print(modelo_hw)
+# Ajuste do modelo de suavização exponencial simples para "SAEBE (N)"
+model_saebe <- ets(data$SAEBE, model = "ANN")
 
-previsao <- forecast::forecast(modelo_hw, h = 3)
-print(previsao)
+# Previsões para 2023 e 2025 para "Indice Aprovação (P)"
+future_years_aprovacao <- c(2023, 2025)
+forecast_aprovacao <- forecast(model_aprovacao, h = length(future_years_aprovacao))
+
+# Previsões para 2023 e 2025 para "SAEBE (N)"
+future_years_saebe <- c(2023, 2025)
+forecast_saebe <- forecast(model_saebe, h = length(future_years_saebe))
+
+# Criação do data frame com as previsões
+future_data <- data.frame(Ano = future_years_aprovacao, Indice_Aprovacao = forecast_aprovacao$mean,
+                          SAEBE = forecast_saebe$mean)
+
+# Concatenando os dados históricos com as previsões
+forecast_data <- rbind(data, future_data)
+
+# Plot dos resultados
+plot(forecast_data$Ano, forecast_data$Indice_Aprovacao, type = "o", pch = 16, col = "blue",
+     xlab = "Ano", ylab = "Indice Aprovação (P)", main = "Previsões para Índice Aprovação (P) e SAEBE (N) em 2023 e 2025",
+     ylim = c(0, max(forecast_data$Indice_Aprovacao, forecast_data$SAEBE) + 0.1))
+lines(forecast_data$Ano, forecast_data$SAEBE, type = "o", pch = 16, col = "orange")
+legend("bottomright", legend = c("Indice Aprovação (P)", "SAEBE (N)"), col = c("blue", "orange"), pch = 16)
+grid()
---
+++ hw_prev_p_n_plot.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+
+# Dados fornecidos
+ano = [2005, 2007, 2009, 2011, 2013, 2015, 2017, 2019, 2021]
+indice_aprovacao = [0.70, 0.74, 0.83, 0.87, 0.91, 0.92, 0.93, 0.95, 0.97]
+saebe = [4.92, 5.14, 5.44, 5.53, 5.71, 5.54, 5.69, 5.65, 5.15]
+ideb = [3.4, 3.8, 4.5, 4.8, 5.2, 5.1, 5.3, 5.3, 5.0, 4.9, 4.7]
+
+# Criação de um DataFrame com os dados
+data = pd.DataFrame({'Ano': ano, 'Indice Aprovação': indice_aprovacao, 'SAEBE': saebe})
+
+# Ajuste do modelo de Holt-Winters aditivo para "Indice Aprovação (P)"
+model_aprovacao = ExponentialSmoothing(data['Indice Aprovação'], seasonal_periods=2, trend='add', seasonal='add')
+fit_model_aprovacao = model_aprovacao.fit()
+
+# Ajuste do modelo de Holt-Winters aditivo para "SAEBE (N)"
+model_saebe = ExponentialSmoothing(data['SAEBE'], seasonal_periods=2, trend='add', seasonal='add')
+fit_model_saebe = model_saebe.fit()
+
+# Previsões para 2023 e 2025 para "Indice Aprovação (P)"
+future_years_aprovacao = [2023, 2025]
+forecast_aprovacao = fit_model_aprovacao.forecast(steps=len(future_years_aprovacao))
+
+# Previsões para 2023 e 2025 para "SAEBE (N)"
+future_years_saebe = [2023, 2025]
+forecast_saebe = fit_model_saebe.forecast(steps=len(future_years_saebe))
+
+# Criação do DataFrame com as previsões
+future_data = pd.DataFrame({'Ano': future_years_aprovacao, 'Indice Aprovação': forecast_aprovacao,
+                            'SAEBE': forecast_saebe})
+
+# Concatenando os dados históricos com as previsões
+forecast_data = pd.concat([data, future_data])
+
+# Plot dos resultados
+plt.figure(figsize=(10, 6))
+plt.plot(forecast_data['Ano'], forecast_data['Indice Aprovação'], marker='o', label='Indice Aprovação (P)')
+plt.plot(forecast_data['Ano'], forecast_data['SAEBE'], marker='o', label='SAEBE (N)')
+plt.plot(ano + [2023, 2025], ideb, marker='o', label='IDEB', linestyle='--', color='red')
+plt.xlabel('Ano')
+plt.ylabel('Valores')
+plt.title('Previsões para Indice Aprovação (P), SAEBE (N) e IDEB em 2023 e 2025')
+plt.legend()
+plt.grid(True)
+plt.show()
