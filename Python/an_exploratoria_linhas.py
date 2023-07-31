0609b17dc23fb97d1ec5a80909dd916fd6a9e35d
antonio <avsj@me.com>
2023-07-31 17:16:33 -0300

    atualizacao

--- Python/an_exploratoria_linhas.py
+++ Python/an_exploratoria_linhas.py
@@ -2,10 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+IR = 'Indicador de Rendimento (P)'
+SAEB = 'SAEB (N)'
 data = {
     'Ano': [2005, 2007, 2009, 2011, 2013, 2015, 2017, 2019, 2021, 2023, 2025],
-    'Indicador de Rendimento (P)': [0.70, 0.74, 0.83, 0.87, 0.91, 0.92, 0.93, 0.95, 0.97, 0.98, 1.00],
-    'SAEB (N)': [4.92, 5.14, 5.44, 5.53, 5.71, 5.54, 5.69, 5.65, 5.15, 5.00, 4.74],
+    IR: [0.70, 0.74, 0.83, 0.87, 0.91, 0.92, 0.93, 0.95, 0.97, 0.98, 1.00],
+    SAEB: [4.92, 5.14, 5.44, 5.53, 5.71, 5.54, 5.69, 5.65, 5.15, 5.00, 4.74],
     'IDEB': [3.4, 3.8, 4.5, 4.8, 5.2, 5.1, 5.3, 5.3, 5.0, 4.9, 4.7]
 }
 
@@ -15,8 +17,8 @@ print(df)
 
 plt.figure(figsize=(12, 6))
 
-plt.plot(df['Ano'], df['Indicador de Rendimento (P)'], marker='o', label='Indicador de Rendimento (P)')
-plt.plot(df['Ano'], df['SAEB (N)'], marker='o', label='SAEB (N)')
+plt.plot(df['Ano'], df[IR], marker='o', label=IR)
+plt.plot(df['Ano'], df[SAEB], marker='o', label=SAEB)
 plt.plot(df['Ano'], df['IDEB'], marker='o', label='IDEB')
 
 plt.xlabel('Ano')
